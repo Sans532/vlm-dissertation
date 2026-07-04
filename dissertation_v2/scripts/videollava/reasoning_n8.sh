9831ea4f4b1812f70a8ff7139e1b930a39a5955b
Sanskruti Kakade <s2850007@hastings.inf.ed.ac.uk>
2026-07-04 09:21:08 +0100

    new files

--- dissertation_v2/scripts/videollava/reasoning_n8.sh
+++ dissertation_v2/scripts/videollava/reasoning_n8.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=RV_n8
 #SBATCH --partition=Teaching
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:h200_3g.71gb:1
 #SBATCH --time=12:00:00
 #SBATCH --mem=60G
 #SBATCH --cpus-per-task=4
